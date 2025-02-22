aa72384c01e005761a073a8367f07ca181bfc75c
Gallego09 <163930532+Gallego09@users.noreply.github.com>
2025-02-21 22:03:05 -0500

    Delete Unit_test directory

--- Unit_test/casos_pruebas/calculos.py
+++
@@ -1,5 +0,0 @@
-# casos_pruebas/calculos.py
-def calcular_monto(P, r, n, d):
-    if r == 0:  # Caso sin interés
-        return P + (d * n)
-    return P * (1 + r) ** n + d * (((1 + r) ** n - 1) / r)
--- Unit_test/casos_pruebasss.py
+++
@@ -1,7 +0,0 @@
-def calcular_monto(P, r, n, d):
-    if r == 0:  # Caso sin interés
-        return P + (d * n)
-    return P * (1 + r) ** n + d * (((1 + r) ** n - 1) / r)
-
-
-
