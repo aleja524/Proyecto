57986e4839da26cb09d95c039625407a2f7cab93
Kevin Mateo Gomez <165510156+KevinMateoG@users.noreply.github.com>
2025-04-08 09:45:15 -0500

    Update erros.py

--- src/model/erros.py
+++ src/model/erros.py
@@ -9,7 +9,6 @@ class aporte_periodico_mayor_a_60( Exception ):
    """El monto de los periodos debe ser menor de 60"""
    pass
 
-
 class tasa_interes_negativa(Exception):
     """No puede ser negativa la tasa de intereses """
     pass
