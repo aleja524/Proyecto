2dd7af57beb3804efd791a40ca687d84ff551196
Kevin Mateo Gomez <gomezdiezkevinmateo@gmail.com>
2025-04-14 16:05:55 -0500

    cambio del uso del sys en la intefaz, y actualizaicon de archivos a ignorar

--- src/view/gui/interfaz.py
+++ src/view/gui/interfaz.py
@@ -1,6 +1,16 @@
 import sys
-sys.path.append("src")
-from model.logic import *
+import os
+
+def resource_path(relative_path):
+    try:
+        base_path = sys._MEIPASS
+    except Exception:
+        base_path = os.path.abspath(".")  
+    return os.path.join(base_path, relative_path)
+
+sys.path.append(resource_path("src"))
+
+from model.logic import *  
 
 
 from kivy.app import App
