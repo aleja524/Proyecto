bb59c4789e87068a602d8fa3ace584183f6b6d0c
Andres_Gallego <agallego892soyudemedellin.edu.co>
2025-05-09 15:05:03 -0500

    Controller and install psycopg2

---
+++ SecretConfig.py
@@ -0,0 +1,6 @@
+# Do not expose your Neon credentials to the browser
+
+PGHOST='ep-tiny-dream-a4pouibw-pooler.us-east-1.aws.neon.tech'
+PGDATABASE='neondb'
+PGUSER='neondb_owner'
+PGPASSWORD='npg_AvHqTn5PojI0'
